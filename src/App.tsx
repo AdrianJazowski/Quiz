f22904f37890a8c4d596ef577d61444328e4f6dc
adrian.jazowski@gmail.com <adrian.jazowski@gmail.com>
2020-10-10 16:55:50 +0200

    add images and simple random function in utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { QuestionCard } from "./components/QuestionCard";
+import {fetchQuizQuestions} from './API'
+import {QuestionState,Difficulty} from './API'
 
-function App() {
-  return <div className="App">quiz</div>;
+
+type AnswerObject= {
+  question: string;
+  asnwer:string;
+  correct:boolean;
+  correctAnswer: string;
 }
+
+const TOTAL_QUESTIONS = 10; 
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestion] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.EASY));
+  
+
+  const startQuiz = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className="App">
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startQuiz}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions ...</p>
+      {/* <QuestionCard
+        questionNr={number + 1}
+        totalQuestion={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className="next" onClick={nextQuestion}>NextQuestion</button>
+    </div>
+  );
+};
 
 export default App;
